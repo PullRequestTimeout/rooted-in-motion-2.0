69ba2c94bb3c46d1c382b7f9fd980f6899358e67
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2025-06-03 09:51:04 -0700

    :construction: Improve CSS naming

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,21 +7,21 @@
 <SeoHead />
 
 <main>
-	<section class="home-hero">
-		<div class="home-hero__text">
-			<h2 class="home-hero__title">
-				Providing <span>effective<img src="/assets/images/svgs/chalk-1.svg" class="home-hero__chalk" alt="chalk outline" /></span> and holistic care for
-				people in pain
+	<section class="hero">
+		<div class="hero-text">
+			<h2 class="hero-title">
+				Providing <span>effective<img src="/assets/images/svgs/chalk-1.svg" class="hero-chalk" alt="chalk outline" /></span> and holistic care for people
+				in pain
 			</h2>
 
 			<p>Using a dynamic and gentle approach I aim to improve my client’s pain management, movement, and general health.</p>
 			<a class="button button-primary" href="https://madhucollective.janeapp.com/#/staff_member/42">Book Session</a>
 		</div>
-		<div class="home-hero__image">
-			<img class="home-hero__svg1 svg" src="/assets/images/svgs/plant-1.svg" alt="plant svg" />
-			<img class="home-hero__svg2 svg" src="/assets/images/svgs/plant-2.svg" alt="plant svg" />
-			<img class="home-hero__svg3 svg" src="/assets/images/svgs/plant-3.svg" alt="plant svg" />
-			<img class="home-hero__svg4 svg" src="/assets/images/svgs/plant-4.svg" alt="plant svg" />
+		<div class="hero-image">
+			<img class="hero-svg1 svg" src="/assets/images/svgs/plant-1.svg" alt="plant svg" />
+			<img class="hero-svg2 svg" src="/assets/images/svgs/plant-2.svg" alt="plant svg" />
+			<img class="hero-svg3 svg" src="/assets/images/svgs/plant-3.svg" alt="plant svg" />
+			<img class="hero-svg4 svg" src="/assets/images/svgs/plant-4.svg" alt="plant svg" />
 
 			<picture>
 				<source srcset="/assets/images/treatment-1.webp" type="image/webp" />
@@ -30,10 +30,12 @@
 			</picture>
 		</div>
 	</section>
+	<section class="symptoms"></section>
 </main>
 
 <style>
-	.home-hero {
+	/* Hero ------------------------------------------------------------------------------------ */
+	section.hero {
 		padding: var(--padding-block) var(--padding-inline);
 		display: grid;
 		grid-template-columns: 1fr;
@@ -41,7 +43,7 @@
 		gap: 1em;
 	}
 
-	.home-hero__text {
+	div.hero-text {
 		margin-top: 5em;
 		display: flex;
 		flex-direction: column;
@@ -49,18 +51,18 @@
 		margin-bottom: 1em;
 	}
 
-	.home-hero__title span {
+	h2.hero-title span {
 		position: relative;
 	}
 
-	.home-hero__title {
+	h2.hero-title {
 		font-size: 2.5em;
 		font-weight: 300;
 		line-height: 1.3;
 		z-index: +1;
 	}
 
-	.home-hero__chalk {
+	img.hero-chalk {
 		position: absolute;
 		left: 0;
 		top: 0;
@@ -69,122 +71,122 @@
 		transform: translate(-0.1rem, -0.2rem);
 	}
 
-	.home-hero__text p {
+	div.hero-text p {
 		font-size: 1.3em;
 		line-height: 1.6;
 		font-weight: 300;
 	}
 
-	.home-hero__image {
+	div.hero-image {
 		justify-self: center;
 		position: relative;
 	}
 
-	.home-hero__image picture img {
+	div.hero-image picture img {
 		filter: drop-shadow(0px 0px 1em var(--color-bronze-2));
 		width: 15em;
 	}
 
-	.home-hero__image .svg {
+	div.hero-image .svg {
 		position: absolute;
 	}
 
-	.home-hero__svg1 {
+	img.hero-svg1 {
 		right: 11em;
 		width: 3em;
 	}
 
-	.home-hero__svg2 {
+	img.hero-svg2 {
 		right: 12em;
 		top: 1em;
 		width: 6em;
 	}
 
-	.home-hero__svg3 {
+	img.hero-svg3 {
 		right: 13em;
 		top: 4em;
 		width: 8em;
 	}
 
-	.home-hero__svg4 {
+	img.hero-svg4 {
 		right: 12em;
 		top: 10em;
 		width: 9em;
 	}
 
 	@media screen and (min-width: 640px) {
-		.home-hero {
+		section.hero {
 			gap: 2em;
 		}
 
-		.home-hero__text {
+		div.hero-text {
 			text-align: center;
 			align-items: center;
 			margin-bottom: unset;
 		}
 
-		.home-hero__image {
+		div.hero-image {
 			width: unset;
 		}
 	}
 
 	@media screen and (min-width: 1024px) {
-		.home-hero {
+		section.hero {
 			display: grid;
 			margin-top: 0;
 			grid-template-columns: 1fr 1fr;
 		}
 
-		.home-hero__text {
+		div.hero-text {
 			text-align: left;
 			align-items: flex-start;
 			gap: 1em;
 		}
 
-		.home-hero__title {
+		h2.hero-title {
 			font-size: 3em;
 			grid-column-start: 1;
 		}
 
-		.home-hero__text p {
+		div.hero-text p {
 			font-size: 1.6em;
 		}
 
-		.home-hero__title,
-		.home-hero__text p {
+		h2.hero-title,
+		div.hero-text p {
 			padding: 0;
 		}
 
-		.home-hero__image {
+		div.hero-image {
 			justify-self: end;
 			align-self: center;
 			margin-top: 4em;
 			grid-column-start: 2;
 		}
 
-		.home-hero__image picture img {
+		div.hero-image picture img {
 			width: 20em;
 		}
 
-		.home-hero__svg1 {
+		img.hero-svg1 {
 			right: 14em;
 			top: -1em;
 			width: 5em;
 		}
 
-		.home-hero__svg2 {
+		img.hero-svg2 {
 			right: 16em;
 			top: 2em;
 			width: 7em;
 		}
 
-		.home-hero__svg3 {
+		img.hero-svg3 {
 			right: 17em;
 			top: 6em;
 			width: 11em;
 		}
 
-		.home-hero__svg4 {
+		img.hero-svg4 {
 			right: 16em;
 			top: 15em;
 			width: 10em;
@@ -192,33 +194,33 @@
 	}
 
 	@media screen and (min-width: 1280px) {
-		.home-hero__title {
+		h2.hero-title {
 			font-size: 3.5em;
 		}
 
-		.home-hero__image picture img {
+		div.hero-image picture img {
 			width: 23em;
 		}
 
-		.home-hero__svg1 {
+		img.hero-svg1 {
 			right: 16em;
 			top: -1em;
 			width: 6em;
 		}
 
-		.home-hero__svg2 {
+		img.hero-svg2 {
 			right: 18em;
 			top: 3em;
 			width: 9em;
 		}
 
-		.home-hero__svg3 {
+		img.hero-svg3 {
 			right: 20em;
 			top: 7em;
 			width: 11em;
 		}
 
-		.home-hero__svg4 {
+		img.hero-svg4 {
 			right: 18em;
 			top: 16em;
 			width: 12em;
@@ -226,38 +228,43 @@
 	}
 
 	@media screen and (min-width: 1800px) {
-		.home-hero__title {
+		h2.hero-title {
 			font-size: 5em;
 		}
 
-		.home-hero__image picture img {
+		div.hero-image picture img {
 			width: 27em;
 		}
 
-		.home-hero__svg1 {
+		img.hero-svg1 {
 			right: 19em;
 			top: -1em;
 			width: 7em;
 		}
 
-		.home-hero__svg2 {
+		img.hero-svg2 {
 			right: 21em;
 			top: 2em;
 			width: 12em;
 			transform: rotate(-20deg);
 		}
 
-		.home-hero__svg3 {
+		img.hero-svg3 {
 			right: 23em;
 			top: 10em;
 			width: 13em;
 			transform: rotate(-10deg);
 		}
 
-		.home-hero__svg4 {
+		img.hero-svg4 {
 			right: 22em;
 			top: 21em;
 			width: 12em;
 		}
+	}
+
+	/* Symptoms -------------------------------------------------------------------------------- */
+	section.symptoms {
+		background-color: var(--color-greyscale-1);
 	}
 </style>
