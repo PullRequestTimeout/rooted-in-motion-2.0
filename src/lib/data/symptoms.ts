c17804014c959907104d0f870bf23d7f9e59c2f1
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2025-06-04 12:23:05 -0700

    :construction: Symptoms data

---
+++ src/lib/data/symptoms.ts
@@ -0,0 +1,51 @@
+export const symptoms = [
+	{
+		title: "Headaches and Neurological Conditions",
+		description:
+			"Manual therapy works with muscles, joints, and fascia (connective tissue surrounding muscles, joints, and organs) of the head, neck, and spine, may help decrease the frequency of headaches and migraines, and may help with neurological symptoms like tinnitus, vertigo, and insomnia and help with rehabilitation after concussions."
+	},
+	{
+		title: "Traumatic Injuries and Whiplash",
+		description:
+			"Manual therapy can help with rehabilitation after car accidents, whiplash injuries, falls, fractures, concussions, and treatment of scar tissue post surgery."
+	},
+	{
+		title: "Sports Injuries",
+		description:
+			"Manual therapy may benefit injuries associated with contact sports and overuse, rehabilitation after ligament tears, sprains, and tendon or capsule injuries."
+	},
+	{
+		title: "Back or Neck Pain",
+		description:
+			"Treatment may benefit those with scoliosis, disc injuries and herniations, arthritis, sciatica, restless leg syndrome, people working in jobs with repetitive movements or requiring long periods of sitting, falls on the back, hips, or tailbone."
+	},
+	{
+		title: "Arthritis",
+		description:
+			"Treatment may help decrease inflammation, improve circulation surrounding affected joints, and decrease muscle tightness, and is safe for those with osteoarthritis and rheumatoid arthritis."
+	},
+	{
+		title: "Joint or Muscle Pain",
+		description:
+			"Manual therapy may help loosen restricted or tight muscles, joints, and fascia (connective tissue surrounding muscles, joints, and organs) and can lubricate joint spaces and decrease inflammation. I work with clients who have connective tissue disorders, joint inflammation (osteoarthritis and rheumatoid arthritis), plantar fasciitis, frozen shoulder, bursitis, numbness and tingling in the extremities, muscle spasms, and carpal tunnel syndrome."
+	},
+	{
+		title: "TMJ Pain",
+		description:
+			"Manual therapy can be related to tight muscles in the face, neck, shoulders, and pelvis and can be related to teeth grinding, history of dental work or dental surgeries, and frequent sinus or ear infections."
+	},
+	{
+		title: "Digestive Concerns",
+		description:
+			"Manual therapy may help with bloating, gas, cramping, hiatal hernias, acid reflux, indigestion, and digestive motility issues like constipation or diarrhea."
+	},
+	{
+		title: "Women's Health Concerns",
+		description: "Manual therapy may help with PMS, abdominal and back pain during menses, PCOS, and endometriosis."
+	},
+	{
+		title: "And more!",
+		description:
+			"I work with clients of all ages and stages of life including perinatal (prenatal and postnatal) clients, newborns, and pediatric clients. Treatments can help prepare the body for labor and work with symptoms associated with pregnancy like muscle and joint pain, nausea, and swelling in the legs. In the postpartum period I work with people who are having breastfeeding difficulties, and babies struggling with gastrointestinal concerns like reflux, gas, and colic or musculoskeletal concerns like torticollis or plagiocephaly. Manual therapy can also help reduce stress and induce relaxation."
+	}
+];
