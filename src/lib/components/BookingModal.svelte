a9c36c7a48fa4d220ef815ad972dc21bf40e3056
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2025-08-27 12:10:27 -0700

    :fire: Removed scroll prevention

--- src/lib/components/BookingModal.svelte
+++ src/lib/components/BookingModal.svelte
@@ -5,31 +5,20 @@
 </script>
 
 {#if uiStore.bookingModalOpen}
-	<div class="modal" class:open={uiStore.bookingModalOpen} transition:fade={{ duration: 100 }}>
+	<div class="modal" class:open={uiStore.bookingModalOpen} transition:fade={{ duration: 200 }}>
 		<div class="modal-content surface" use:clickOutside onoutclick={closeBookingModal}>
 			<button class="close" onclick={closeBookingModal}><span class="material-icons">close</span></button>
-			<h2 out:fade in:fade={{ delay: 1 * 100 }}>Book Session:</h2>
-			<a out:fade in:fade={{ delay: 1.25 * 100 }} href="https://madhucollective.janeapp.com/#/staff_member/42">Rossland</a>
-			<hr out:fade in:fade={{ delay: 1.5 * 100 }} />
-			<a out:fade in:fade={{ delay: 1.75 * 100 }} href="https://spectrumtreatmentcentre.janeapp.com/#/staff_member/29">Castlegar</a>
-			<hr out:fade in:fade={{ delay: 2 * 100 }} />
-			<a out:fade in:fade={{ delay: 2.25 * 100 }} href="/contact">Virtual</a>
+			<h2 out:fade in:fade={{ delay: 2 * 100 }}>Book Session:</h2>
+			<a out:fade in:fade={{ delay: 2.25 * 100 }} href="https://madhucollective.janeapp.com/#/staff_member/42">Rossland</a>
+			<hr out:fade in:fade={{ delay: 2.5 * 100 }} />
+			<a out:fade in:fade={{ delay: 2.75 * 100 }} href="https://spectrumtreatmentcentre.janeapp.com/#/staff_member/29">Castlegar</a>
+			<hr out:fade in:fade={{ delay: 3 * 100 }} />
+			<a out:fade in:fade={{ delay: 3.25 * 100 }} href="/contact">Virtual</a>
 		</div>
 	</div>
 {/if}
 
 <style>
-	:global(body:has(div.modal.open)) {
-		overflow: hidden;
-		max-height: 100vh;
-	}
-
-	@media (min-width: 1024px) {
-		:global(body:has(div.modal.open) header nav) {
-			translate: -1rem 0;
-		}
-	}
-
 	div.modal {
 		position: fixed;
 		z-index: 1000;
