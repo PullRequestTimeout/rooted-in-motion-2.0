b1c9dc0ba3181662582110d070848c9be131c920
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2025-09-02 09:44:42 -0700

    :recycle: Refactor out top level await for legacy enviroments

--- src/lib/utils/refreshBlogPosts.svelte.ts
+++ src/lib/utils/refreshBlogPosts.svelte.ts
@@ -2,10 +2,12 @@ import type { BlogPost } from "$lib/components/blog/blogOutput.svelte";
 import { databaseHandlers } from "$lib/firebase/db";
 import { invalidateAll } from "$app/navigation";
 
-export let blogPostsArr: BlogPost[] = $state(await databaseHandlers.getAllBlogPosts());
+export let blogPostsArr: BlogPost[] = $state([]);
 
 export async function refreshBlogPosts() {
 	const localBlogPostsArr = await databaseHandlers.getAllBlogPosts();
 	blogPostsArr.splice(0, blogPostsArr.length, ...localBlogPostsArr.sort((a: BlogPost, b: BlogPost) => b.date.localeCompare(a.date)));
 	invalidateAll();
 }
+
+refreshBlogPosts();
